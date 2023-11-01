5644379212ba6256cef12b25e63c1c3bc0e8a1f8
abilov042 <shamilabilov599@gmail.com>
2023-11-01 20:35:33 +0400

    first commit

---
+++ src/main/java/com/task/bookstore/entity/concretes/users/User.java
@@ -0,0 +1,24 @@
+package com.task.bookstore.entity.concretes.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+}
