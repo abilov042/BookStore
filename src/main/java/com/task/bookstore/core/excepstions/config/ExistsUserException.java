2c3ec05221054c22c44b3defae105f1d6a401685
Shamil Abilov <shamil.abilov@digigo.work>
2026-02-15 11:55:09 +0400

    add some files

--- src/main/java/com/task/bookstore/core/excepstions/config/ExistsUserException.java
+++ src/main/java/com/task/bookstore/core/excepstions/config/ExistsUserException.java
@@ -3,5 +3,6 @@ package com.task.bookstore.core.excepstions.config;
 public class ExistsUserException extends RuntimeException{
     public ExistsUserException(String message){
         super(message);
+
     }
 }
